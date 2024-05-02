8082ead4ce8c3c6f678e0a680980b858755d20dd
mouadedd <mdx.edd@gmail.com>
2024-05-02 04:08:20 -0700

    tested

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Write a type-annotated function make_multiplier that takes a float
+    multiplier as argument and returns a function that multiplies 
+    a float by multiplier
+    """
+    return lambda x: x * multiplier
