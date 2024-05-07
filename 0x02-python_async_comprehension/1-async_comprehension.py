b68369b58c97afd89bc07385390c81bed3bdaa22
mouadedd <mdx.edd@gmail.com>
2024-05-07 11:32:51 +0000

    changed the typing annotation from Generator to list

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """ Async Comprehensions """
 import asyncio
-from typing import Generator
+from typing import List
 import random
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[int, None,None]:
+async def async_comprehension() -> List[float]:
     """
     The coroutine will collect 10 random numbers using an async comprehensing
     over async_generator, then return the 10 random numbers."""
