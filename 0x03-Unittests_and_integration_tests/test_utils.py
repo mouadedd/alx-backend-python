4e5844287ba3d67231cabf1dd049d4d64a5fdee0
mouadedd <mdx.edd@gmail.com>
2024-05-26 05:28:58 -0700

    mocked

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """a test for utils module's access_nested_map"""
 import unittest
-from utils import access_nested_map as anm, get_json
+from utils import access_nested_map as anm, get_json, memoize
 from typing import Dict, Tuple, Union
 from parameterized import parameterized
 from unittest.mock import patch, Mock
@@ -43,6 +43,26 @@ class TestGetJson(unittest.TestCase):
         with patch("requests.get", return_value=Mock(**args)) as g_rqst:
             self.assertEqual(get_json(test_url), test_payload)
             g_rqst.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """memoization test """
+    def test_memoize(self) -> None:
+        """test memoize and check that it's clled once"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mocked:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mocked.assert_called_once()
 
 
 if __name__ == '__main__':
