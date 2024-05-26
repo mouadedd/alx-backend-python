6da3c1c06974b3b3d4136e923b394ae9b223fdde
mouadedd <mdx.edd@gmail.com>
2024-05-26 04:44:29 -0700

    2-mock http calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """a test for utils module's access_nested_map"""
 import unittest
-from utils import access_nested_map as anm
+from utils import access_nested_map as anm, get_json
 from typing import Dict, Tuple, Union
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,6 +29,20 @@ class TestAccessNestedMap(unittest.TestCase):
         """test a key error input"""
         with self.assertRaises(exp):
             anm(nst, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ mock http calls"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url: str, test_payload: dict) -> None:
+        """ test get_json """
+        args = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**args)) as g_rqst:
+            self.assertEqual(get_json(test_url), test_payload)
+            g_rqst.assert_called_once_with(test_url)
 
 
 if __name__ == '__main__':
